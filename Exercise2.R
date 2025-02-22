12e93ae7f4ccf91c8685bbd3b8d780ff2e97fa58
Eldrich Pereira <pereiraeldrich27@gmail.com>
2025-02-22 19:59:15 +0530

    listing the files with the full path and saving all the paths in a vector

--- Exercise2.R
+++ Exercise2.R
@@ -8,4 +8,11 @@ library(R.utils)
 
 #listing all the files in the Zip folder
 #Filtering by patterns
-list.files(path = 'Zip/',pattern = 'gz')
+list.files(path = 'Zip/',pattern = 'gz',full.names = T)
+
+# Creating a vector to store all the zipfiles paths
+myzipfiles <- list.files(path = 'Zip/',pattern = 'gz',full.names = T)
+
+#Vectors are not zero indexed like python, the indexes start form 1
+# Accessing the first path in myzipfiles
+myzipfiles[1]
