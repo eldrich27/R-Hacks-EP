f8becc0aae76beec7faef92bea3b9cfb6c3435dd
Eldrich Pereira <pereiraeldrich27@gmail.com>
2025-02-22 19:46:07 +0530

    Importing libraries and listing files

--- Exercise2.R
+++ Exercise2.R
@@ -1,1 +1,11 @@
 ## Understanding loops and map functions
+install.packages('tidyverse')
+install.packages('R.utils')
+
+## importing Libratries
+library(tidyverse)
+library(R.utils)
+
+#listing all the files in the Zip folder
+#Filtering by patterns
+list.files(path = 'Zip/',pattern = 'gz')
